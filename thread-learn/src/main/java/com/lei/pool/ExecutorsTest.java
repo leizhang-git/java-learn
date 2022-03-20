569ecb8acd1f68dba5d1ff872d2824ca0558c92f
zhanglei <vip.p@live.com>
2022-03-20 22:58:56 +0800

    线程池

---
+++ thread-learn/src/main/java/com/lei/pool/ExecutorsTest.java
@@ -0,0 +1,15 @@
+package com.lei.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorsTest {
+    public static void main(String[] args) {
+
+        // 创建线程池
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+        // 执行线程任务(原生创建时采用的是 newLinkedBlockingQueue<Runnable>() 是一个无边界队列，一直加任务，会导致内存问题！)
+        executorService.execute(() -> System.out.println("111"));
+    }
+}
